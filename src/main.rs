bdc1ef4be6fa594aa7d5446e84061e2978b5e0f5
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2022-03-01 22:01:58 +0100

    Draw some basic hexagons.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::f64::consts::PI;
 
 use svg::node::element::path::Data;
-use svg::node::element::Path;
+use svg::node::element::{Group, Path};
 use svg::Document;
 
-const HEX_RADIUS: f64 = 120.0;
+const SQRT_3: f64 = 1.73205080757;
+
+const HEX_WIDTH: f64 = 200.0;
+const HEX_RADIUS: f64 = (HEX_WIDTH / 2.0) * 2.0 / SQRT_3;
+
+const INNER_SPACE: f64 = 20.0;
 
 // Returns data for a hexagon centered on (0, 0) with the given radius.
 // Flat sides are parallel to the y-axis.
@@ -27,15 +32,44 @@ fn outline(data: Data) -> Path {
         .set("fill", "none")
 }
 
+trait Transformable: Sized {
+    /// Arbitrary translation.
+    fn translate(self, x: f64, y: f64) -> Self;
+    /// Translation on a hex grid.
+    fn hex_translate(self, x: i64, y: i64) -> Self {
+        self.translate(
+            (x as f64 + y as f64 / 2.0) * (HEX_WIDTH + INNER_SPACE),
+            y as f64 * (HEX_WIDTH + INNER_SPACE) / 2.0 * SQRT_3,
+        )
+    }
+}
+
+impl Transformable for Group {
+    fn translate(self, x: f64, y: f64) -> Self {
+        self.set("transform", format!("translate({}, {})", x, y))
+    }
+}
+
 fn main() {
     // let path1 = outline(hex_data(40.0, 5.0));
 
-    let path2 = outline(hex_data(HEX_RADIUS));
+    let mut document = Document::new().set("viewBox", (-400, -400, 800, 800)); // x, y, w, h
 
-    let document = Document::new()
-        .set("viewBox", (-400, -400, 800, 800)) // x, y, w, h
-        // .add(path1)
-        .add(path2);
+    for i in -1..=1 {
+        let path2 = outline(hex_data(HEX_RADIUS));
+        let g = Group::new()
+            .add(path2)
+            //.translate((i as f64) * (HEX_WIDTH + INNER_SPACE), 0.0);
+            .hex_translate(0, i);
+        document = document.add(g);
+
+        let path2 = outline(hex_data(HEX_RADIUS));
+        let g = Group::new()
+            .add(path2)
+            //.translate((i as f64) * (HEX_WIDTH + INNER_SPACE), 0.0);
+            .hex_translate(i, 0);
+        document = document.add(g);
+    }
 
     svg::save("output/image.svg", &document).unwrap();
 
