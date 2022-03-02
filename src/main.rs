1d42b84a42e7832a3421e9aec6644d4b26e3b36d
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2022-03-02 18:09:44 +0100

    Curved hexagons

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,15 @@ use svg::Document;
 
 // Math Constants
 const SQRT_3: f64 = 1.73205080757;
+const TAN_30: f64 = 0.57735026919;
 
 // Size Constants
 const HEX_WIDTH: f64 = 200.0;
 const HEX_RADIUS: f64 = (HEX_WIDTH / 2.0) * 2.0 / SQRT_3;
 
 const INNER_SPACE: f64 = 20.0;
+const CORNER_INSET: f64 = INNER_SPACE * TAN_30;
+const CORNER_INSET_REL: f64 = CORNER_INSET / HEX_RADIUS;
 
 // Design Constants
 const TOP_WIDTH: i64 = 5;
@@ -19,15 +22,31 @@ const HEIGHT: i64 = 3;
 
 // Returns data for a hexagon centered on (0, 0) with the given radius.
 // Flat sides are parallel to the y-axis.
+#[allow(clippy::needless_range_loop)] // It is easier to read this way.
 fn hex_data(radius: f64) -> Data {
+    let mut hex_points: [(f64, f64); 12] = [(0.0, 0.0); 12];
+    for i in 0..12 {
+        let angle = i as f64 * PI / 3.0 + PI / 6.0;
+        hex_points[i] = (radius * angle.cos(), radius * angle.sin());
+    }
+
     let mut data = Data::new();
-    let mut angle: f64 = PI / 6.0;
-    data = data.move_to((radius * angle.cos(), radius * angle.sin()));
-    for _ in 1..7 {
-        data = data.line_to((radius * angle.cos(), radius * angle.sin()));
-        angle += PI / 3.0;
+    data = data.move_to(lerp(hex_points[0], hex_points[1], CORNER_INSET_REL));
+    for i in 1..7 {
+        // Slightly shorter outer line where we substract relative corner inset.
+        let c1 = lerp(hex_points[i - 1], hex_points[i], (1.0 - CORNER_INSET_REL));
+        data = data.line_to(c1);
+        // And now we already step into the next side.
+        // But instead of line_to, we'll sweep with an arc.
+        let (c2x, c2y) = lerp(hex_points[i], hex_points[i + 1], CORNER_INSET_REL);
+        // data = data.line_to(c2);
+        data = data.elliptical_arc_to((INNER_SPACE, INNER_SPACE, 0.0, 0.0, 1.0, c2x, c2y));
     }
     data.close()
+}
+
+fn lerp(a: (f64, f64), b: (f64, f64), t: f64) -> (f64, f64) {
+    (a.0 + (b.0 - a.0) * t, a.1 + (b.1 - a.1) * t)
 }
 
 fn outline(data: Data) -> Path {
